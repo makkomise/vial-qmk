3b568a3de43f985cfceea000df5a5d929193d957
makkomise <57153670+makkomise@users.noreply.github.com>
2025-03-06 17:53:29 +0200

    initial push
    added keymaps and configs

---
+++ keyboards/slothie60/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x96, 0x52, 0xCB, 0x69, 0xA6, 0x2B, 0x80, 0x3A}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
