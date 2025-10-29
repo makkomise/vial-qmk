1e30c62db2a5966e8a3461de85649217d37e7bef
Mikko Turunen <57153670+makkomise@users.noreply.github.com>
2025-10-29 15:57:02 +0200

    Update to diydeck
    - Adjusted keymaps
    - Added one more hotkey

--- keyboards/makkomise/diydeck/keymaps/vial/keymap.c
+++ keyboards/makkomise/diydeck/keymaps/vial/keymap.c
@@ -4,7 +4,7 @@
 #include QMK_KEYBOARD_H
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT( 
-	KC_P0,	KC_P1,	KC_P2,	KC_P3,	KC_P4,	KC_P5,	KC_P6 )
+	KC_P0,	KC_P1,	KC_P2,	KC_P3,	KC_P4,	KC_P5,	KC_P6, 	KC_P7 )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
