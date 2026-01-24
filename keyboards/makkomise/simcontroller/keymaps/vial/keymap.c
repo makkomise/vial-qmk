280526d3eec82800d559aa091cfb0d04dca04de4
Mikko Turunen <57153670+makkomise@users.noreply.github.com>
2026-01-24 05:18:22 +0200

    idk

--- keyboards/makkomise/simcontroller/keymaps/vial/keymap.c
+++ keyboards/makkomise/simcontroller/keymaps/vial/keymap.c
@@ -5,9 +5,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT( 
-		KC_SPACE,	KC_L,	KC_U,	KC_Y,
-					KC_O,	KC_J, KC_K,	
-		KC_E,		KC_F,	KC_N,	KC_M,
-		KC_Q,		KC_T,	KC_P			
+	KC_SPACE,	KC_L,	KC_U,	KC_Y,
+				KC_O,	KC_J, KC_K,	
+	KC_E,		KC_F,	KC_N,	KC_M,
+	KC_Q,		KC_T,	KC_P			
 	)	
 };
